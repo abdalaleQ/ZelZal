2ba12d23a091daf3b3d30fe172de54e84987d608
abdalaleQ <153198040+abdalaleQ@users.noreply.github.com>
2023-12-30 05:45:26 +0300

    Update ايدي.py

--- zthon/plugins/ايدي.py
+++ zthon/plugins/ايدي.py
@@ -37,12 +37,12 @@ from . import spamwatch
 plugin_category = "العروض"
 LOGS = logging.getLogger(__name__)
 # code by t.me/zzzzl1l
-ZED_TEXT = gvarstatus("CUSTOM_ALIVE_TEXT") or "•⎚• مـعلومـات المسـتخـدم مـن بـوت زدثــون"
+ZED_TEXT = gvarstatus("CUSTOM_ALIVE_TEXT") or "•⎚• مـعلومـات المسـتخـدم مـن بـوت الزعيم"
 ZEDM = gvarstatus("CUSTOM_ALIVE_EMOJI") or "✦ "
 ZEDF = gvarstatus("CUSTOM_ALIVE_FONT") or "⋆─┄─┄─┄─ ᶻᵗʰᵒᶰ ─┄─┄─┄─⋆"
-zed_dev = (5176749470, 1895219306, 925972505, 5280339206, 5426390871)
-zel_dev = (5176749470, 5426390871)
-zelzal = (925972505, 1895219306, 5280339206)
+zed_dev = (6700336976, 6700336976, 6700336976, 6700336976, 6700336976)
+zel_dev = (6700336976, 6700336976)
+zelzal = (6700336976, 6700336976, 5280339206)
 
 
 async def get_user_from_event(event):
